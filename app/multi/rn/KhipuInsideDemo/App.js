1eda7f85884c4157a11c48c305c969db1e446f92
Emilio Davis <emilio.davis@khipu.com>
2022-01-03 12:06:35 -0300

    base rm

--- app/multi/rn/KhipuInsideDemo/App.js
+++ app/multi/rn/KhipuInsideDemo/App.js
@@ -3,65 +3,94 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow
+ * @flow strict-local
  */
 
 import React from 'react';
+import type {Node} from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
+  ScrollView,
   StatusBar,
-  Button,
-  Alert,
-  TextInput,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
 } from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-import {NativeModules} from 'react-native';
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
 
-  startPaymentById = () => {
-    console.log('payment');
-    NativeModules.KhenshinBridge.startPaymentById(this.state.text, result => {
-      Alert.alert('Result', result, [], {cancelable: false});
-    });
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  render() {
-    return (
-      <>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView style={{flexDirection: 'row', flex: 1, margin: 40}}>
-          <TextInput
-            style={{height: 40, flex: 1}}
-            placeholder="PaymentId"
-            onChangeText={text => this.setState({text})}
-            value={this.state.text}
-          />
-          <Button title={'Pay'} onPress={this.startPaymentById} style={{flex: 1}} />
-        </SafeAreaView>
-      </>
-    );
-  }
-}
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -69,24 +98,14 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-    color: Colors.black,
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
-    color: Colors.dark,
   },
   highlight: {
     fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
