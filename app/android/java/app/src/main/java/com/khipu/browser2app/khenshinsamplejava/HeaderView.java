eeed34deadc3570cfd2c2fc4b0a9e56e754af49f
mauriciocastillo <mauricio.castillo@khipu.com>
2024-04-30 17:58:30 -0400

    archivos faltantes

---
+++ app/android/java/app/src/main/java/com/khipu/browser2app/khenshinsamplejava/HeaderView.java
@@ -0,0 +1,14 @@
+package com.khipu.browser2app.khenshinsamplejava;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class HeaderView extends LinearLayout {
+    public HeaderView(Context context) {
+        super(context);
+    }
+
+    public void init() {
+
+    }
+}
---
+++ app/android/java/settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven ( url = "https://dev.khipu.com/nexus/content/repositories/khenshin/" )
+    }
+}
+
+rootProject.name = "Khenshin Sample Java"
+include(":app")
+ 
